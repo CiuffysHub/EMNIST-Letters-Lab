e9ab3790406af92f80fc65f464af0f4497df812a
Marco Guerra <marco.guerra@stud.unifi.it>
2021-03-24 21:58:57 +0100

    Add files via upload

---
+++ decisiontree.py
@@ -0,0 +1,76 @@
+print('Importing...')
+
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import plot_confusion_matrix
+import matplotlib.pyplot as plt
+import time
+import sys
+
+max = 0
+real_labels = []
+
+def decision_tree(train, test, labels_train, labels_test):
+  print('Training DT...')
+  dtree = DecisionTreeClassifier()
+  dtree.fit(train, labels_train)
+  print('Scoring DT...')
+  print('Accuracy:',dtree.score(test, labels_test))
+  return dtree 
+
+if __name__ == "__main__":
+  features = sys.argv[1]
+  labels = sys.argv[2]
+  
+  print('Loading features...')
+
+  data = np.load(features)
+  train_set = data['train']
+  test_set = data['test']
+
+  print('Loading labels...')
+
+  data = np.load(labels)
+  train_labels = data['train']
+  test_labels = data['test']
+  for l in test_labels:
+  	if l>max:
+  		max=l;
+
+  print('Starting training...')
+
+  start = time.perf_counter()
+
+  classifier = decision_tree(train_set, test_set, train_labels, test_labels)
+  print('Done in',time.perf_counter()-start,'seconds!')
+  real_labels = [chr(x) for x in range(65,91)]
+  if max == 27:
+  	real_labels=real_labels+['|']
+
+  fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(18,9))
+
+  disp = plot_confusion_matrix(classifier, test_set, test_labels,
+                             display_labels=real_labels,
+                             cmap=plt.cm.Blues,ax=axes.flatten()[0])
+
+  right = [0]*max
+  wrong = [0]*max
+
+  predicts = classifier.predict(test_set)
+
+  np.savez_compressed('predicts',preds=classifier.predict(test_set))
+
+  for i in range(0,len(test_set)):
+    if test_labels[i] == predicts[i]:
+      right[test_labels[i]-1]=right[test_labels[i]-1]+1
+    else:
+      wrong[test_labels[i]-1]=wrong[test_labels[i]-1]+1
+
+
+  X = np.arange(max)
+
+  plt.bar(X, np.asarray(right), color = 'r')
+  plt.bar(X, -np.asarray(wrong), color = 'b')
+  plt.xticks(X, real_labels)
+  plt.tight_layout()  
+  plt.show()
